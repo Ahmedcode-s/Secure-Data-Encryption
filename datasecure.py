58a218f8e143605e07b15f234ea881bf86a7e5c7
Ahmed Faraz <ahmedcodes8y9@gmail.com>
2025-04-29 03:56:26 +0500

    Update datasecure.py

--- datasecure.py
+++ datasecure.py
@@ -87,7 +87,7 @@ elif choice == "Register":
                 st.success("User register sucessfully!")
         else:
             st.error("Both fields are required.")
-    elif choice == "Login":
+elif choice == "Login":
         st.subheader("User Login")
 
         if time.time() < st.session_state.lockout_time:
